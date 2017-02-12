1062464175a573298e7219c89b10dffa4c44d779
Marani Matias Ezequiel <maranimatias@gmail.com>
2017-02-12 06:52:32 -0800

    fix game/code/imput and add up it to  viwer

--- routes/main.js
+++ routes/main.js
@@ -35,12 +35,11 @@ app.get('/game/:code', function(req, res){
   });
 });
 
-/*
 app.get('/game/:code/:input', function(req, res){
   Games.findOne({ code: req.params.code }, function(err, g){
     g.try(req.params.input, function(err, game){
       res.render('game', { code: game.code, num_players: game.num_players, game: game });
-    });.
+    });
   });
 });
-*/
+

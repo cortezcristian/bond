8bb4ba7367db5985b87cee5d659de3eacbdaa219
Marani Matias Ezequiel <maranimatias@gmail.com>
2017-02-12 02:27:17 -0300

    add         Users.findOrCreate(nickname, function(err, user) {test

--- test/user-tests.js
+++ test/user-tests.js
@@ -13,21 +13,44 @@ var dbConex = exports.dbConex = mongoose.connect('mongodb://localhost/gamedb');
 var User, user;
 
 // Unit Tests
-describe('Model Test User', function(){
-    before(function(){
-        // Before all tests
-        User = require("../models/user.js");
+describe('Model Test User', function() {
+  before(function() {
+    // Before all tests
+    Users = require("../models/user.js");
+  });
+
+  describe('User', function() {
+    // It show create a new document in the database
+    it('add a user', function(done) {
+      var user = new Users({
+        name: 'user' + Math.floor((Math.random() * 10) + 1)
+      });
+      user.save(done);
     });
 
-    describe('User', function(){
-        // It show create a new document in the database
-        it('add a user', function(done){
-            user = new User({ name: 'user'+Math.floor((Math.random() * 10) + 1)});
-            user.save(done);
+    describe('find or create user by nickname', function(done) {
+      var nickname = 'user' + Math.floor((Math.random() * 10) + 1);
+      var userNew;
+      it('New user\'s name ' + nickname, function(done) {
+        Users.findOrCreate(nickname, function(err, user) {
+          if (!err) {
+            userNew = user;
+            done();
+          } else {
+            done(err);
+          }
         });
+      });
 
-        it('find or create user by nickname', function(done){
+      it('Find user\'s name ' + nickname, function(done) {
+        Users.findOrCreate(nickname, function(err, user) {
+          console.log(user._id , userNew._id);
+          assert.equal(err, null /*,handleError(err)*/ );
+          //assert.equal(userNew._id, user._id);
+          done();
         });
-
+      });
     });
+
+  });
 });

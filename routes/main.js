1a06d1df360616e678c8402b7bde4d2f2b444bb9
Marani Matias Ezequiel <maranimatias@gmail.com>
2017-02-12 04:16:48 -0300

    list de plares on shere

--- routes/main.js
+++ routes/main.js
@@ -7,7 +7,7 @@ var app = module.parent.exports.app,
 app.post('/create', function(req, res){
   Users.findOrCreate(req.body.nickname, function(err, user){
     user.createGame(req.body.num_players, function(err, game){
-      res.render('share', { code: game.code, num_players: game.num_players });
+      res.render('share', { code: game.code, num_players: game.num_players, players: game.players });
     });
   });
 });

88c74d8f0bcfa264826d537f31289721c7d9a593
Cristian Cortez <cortez.cristian@gmail.com>
2017-02-11 19:57:10 -0800

    Create user

--- routes/main.js
+++ routes/main.js
@@ -1,10 +1,12 @@
 // Main Routes Here
 var app = module.parent.exports.app,
+  dbConex = module.parent.exports.dbConex,
   Games = require('../models/game.js'),
   Users = require('../models/user.js');
 
 app.post('/create', function(req, res){
-  Users.findOrCreate({ nickname: req.body.nickname}, function(user){
+  console.log(req.body);
+  Users.findOrCreate(req.body.nickname, function(err, user){
     res.json(user);
   });
 });

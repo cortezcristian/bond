e553803b5bfc70f4f70e0ccc48ad0d9f8c65e11f
Marani Matias Ezequiel <maranimatias@gmail.com>
2017-03-13 23:43:40 -0300

    show plays on 'share' view, now only one time

--- routes/socket.js
+++ routes/socket.js
@@ -12,7 +12,7 @@ module.exports = function(io) {
   function removePlayers(id){
     for(var i=0; i< playersConnected.length;i++){
       if(playersConnected[i].socket.id===id){
-        console.log('delete:', playersConnected.slice(i,1)[0].socket.id);
+        console.log('socket disconnected:', playersConnected.slice(i,1)[0].socket.id);
         break;
       }
     }
@@ -22,7 +22,7 @@ module.exports = function(io) {
     var self = socket;
     console.log('->>  sokect connect',socket.id);
     socket.on("disconnect", function() {
-      console.log("socket disconnected", socket.id);
+      //console.log("socket disconnected", socket.id);
       removePlayers(socket.id);
     });
 
@@ -31,9 +31,7 @@ module.exports = function(io) {
       //console.log(playersConnected[0].playerID,playersConnected[0].socket.id);
       Games.findOne({ code: data.gameCode }, function(err, g){
         Games.populate(g, { path: 'players', model: 'User' }, function(err, game){
-          playersConnected[0].socket.emit('joinPlayer-'+data.gameCode, {
-            players:game.players
-          });
+          playersConnected[0].socket.emit('joinPlayer-'+data.gameCode, { players:game.players });
         });
       });
     });
